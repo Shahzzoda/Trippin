ff8b53e58f019b0ec71817e2af03d4d7a96cc58d
Shahzoda Davlatova <shahzoda01@gmail.com>
2019-12-13 13:44:59 -0500

    name on the image cards in map page

--- client/src/pages/MapPage.js
+++ client/src/pages/MapPage.js
@@ -61,7 +61,7 @@ class MapPage extends React.Component {
       .then(posts => {
         this.setState({
           loading: false,
-          posts: posts.map((p,ii) => <ImageCard {...p} key={ii} src={p.coverphoto}/>),
+          posts: posts.map((p,ii) => <ImageCard {...p} key={ii} src={p.coverphoto} tripname={p.name}/>),
         });
       })
       .catch(err => console.log("API ERROR: ", err));
